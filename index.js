4480ff86d2c5487ed51f390b1b27e53d514f586d
Monirul-RS <monir030599@gmail.com>
2022-10-27 02:36:57 +0600

    data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000 ;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const courseDetails = require('./data/courseDetails.json')
 
 
 app.get('/', (req, res) =>{
@@ -14,6 +15,19 @@ app.get('/', (req, res) =>{
 
 app.get('/courses', (req, res) =>{
     res.send(courses);
+});
+
+app.get('/course/:id', (req, res) =>{
+    const id = req.params.id;
+    const course_details = courseDetails.filter(c => c.course_id === id);
+    res.send(course_details) ;
+})
+
+app.get('/courses/:id', (req, res) =>{
+    const id = req.params.id;
+    // console.log(req.params.id);
+    const selectedCourse = courseDetails.find( courseDetail => courseDetail._id === id);
+    res.send(selectedCourse)
 })
 
 app.listen(port, () => {
