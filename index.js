f9fe5e7f61b861eb9fc3e1fbf5ec0ef5808a61af
Monirul-RS <monir030599@gmail.com>
2022-10-26 12:04:42 +0600

    commmittt

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000 ;
+
+app.use(cors());
+
+const courses = require('./data/courses.json');
 
 
 app.get('/', (req, res) =>{
     res.send('Learning Platform running');
 });
+
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log("Learning platform server running on port", port);
